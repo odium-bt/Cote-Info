3c6f24a7f7b3c44b66798fcd77658da592c18ba8
Zadig <odium@riseup.net>
2026-04-03 09:45:39 +0200

    ajout d'un compteur de tentatives de connexions

--- app/Controller/LoginForm.php
+++ app/Controller/LoginForm.php
@@ -15,6 +15,10 @@ class LoginForm
     protected string $password;
     protected int $counter = 0;
 
+    protected float $startTime = 0;
+    protected float $endTime = 0;
+    protected float $elapsedTime = 0;
+
     public function __construct()
     {
         // Contrôle de qualité (trouve les erreurs et ajoute les messages dans le tableau $errors)
@@ -40,35 +44,54 @@ class LoginForm
             }
 
             $user = new UserModel;
-
-            $user->loginCheck($this->email, $this->password);
-
-
-            // ============ Sortie
-            // Si $_POST est vide (début) ou qu'il y a des erreurs, affiche le formulaire
-            if (empty($_POST) || !empty($this->errors)) {
-                require ROOT . '/vue/connexion_succes_vue.php'; // affiche page succès
-            } else {
-
-                $this->errors['email'] = "Cette adresse existe déjà";
-                require ROOT . '/app/View/connexion_view.php'; // Retour au formulaire
-            }
-            // Email
-
-
-            // Mot de passe
-
-
+            if (!$user->loginCheck($this->email, $this->password)) {
+                $this->errors['failure'] = "L'email ou le mot de passe que vous avez entrés sont erronés";
+                $this->incrementCounter();
+            };
         }
-
-
+        // ============ Sortie
         // Si $_POST est vide (début) ou qu'il y a des erreurs, affiche le formulaire
         if (empty($_POST) || !empty($this->errors)) {
-            require ROOT . '/app/View/connexion_view.php';
+            require ROOT . '/app/View/connexion_view.php'; // Retour au formulaire
         }
-        // Sinon $_POST est rempli sans erreurs
+        // Sinon $_POST est rempli sans erreurs, affiche la page succès et connecte l'utilisateur
         else {
+            require ROOT . '/vue/connexion_succes_vue.php'; // Affiche page succès
         }
         // ============
+    }
+
+    /*
+     * Fonction incrementCounter
+     * paramètres : /
+     * résultat : si le compteur est à 10 ou +, ajoute une erreur
+     */
+    private function incrementCounter()
+    {
+        if ($this->counter >= 10) {
+            $this->errors['failure'] = "Trop de tentatives successives, merci d'attendre 10 minutes avant de réessayer";
+            if ($this->elapsedTime <= 0) {
+                $this->startTimer();
+            }
+        } else {
+            $this->counter += 1;
+        }
+    }
+
+    /*
+     * Fonction startTimer
+     * paramètres : /
+     * résultat : commence un décompte; après 10 minutes la variable counter retourne à 0
+     */
+    protected function startTimer()
+    {
+        $this->startTime = microtime(true);
+        while ($this->elapsedTime <= 600) {
+            $this->endTime = microtime(true);
+            $this->elapsedTime = $this->endTime - $this->startTime;
+            usleep(1000000); // waits 1 second
+        }
+        $this->elapsedTime = 0;
+        $this->counter = 0;
     }
 }

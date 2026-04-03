a4cc39f8dc4fa75ff22bc927e00969bb5615c913
Zadig <odium@riseup.net>
2026-04-03 11:48:55 +0200

    fixed a bug with how the form displays errors

--- app/View/connexion_view.php
+++ app/View/connexion_view.php
@@ -2,27 +2,25 @@
     <div class="flex">
         <!-- Formulaire de connexion -->
         <div class="box">
-            <div class="form" action="login" method="post" >
+            <form class="form" action="?action=login" method="post">
                 <h1>Connexion</h1>
+
+                <p><?php if (isset($this->errors["failure"])) echo ("<span class='error'>" . $this->errors["failure"] . "</span>"); ?></p>
+
                 <div class="form-group">
                     <label for="email">Email : </label><br>
                     <input type="email" name="email" id="email"
-                        value=<?php // Garde la valeur si pas d'erreurs, efface si il y en a
-                                if (!isset($Login->errors["email"])) {
-                                    echo $_POST["email"] ?? '';
-                                } else {
-                                    echo '';
-                                }; ?> required>
-                    <?php if (isset($Login->errors['email'])) echo ("<br><span class='error'>" . $Login->errors['email'] . "</span>"); ?>
+                        value="<?php echo $_POST["email"] ?? ''; ?>" required>
+                    <?php if (isset($this->errors['email'])) echo ("<br><span class='error'>" . $this->errors['email'] . "</span>"); ?>
                 </div>
                 <div class="form-group">
                     <label for="password">Mot de passe : </label><br>
                     <input type="password" name="password" id="password" required>
-                    <?php if (isset($Login->errors["password"])) echo ("<br><span class='error'>" . $Login->errors["password"] . "</span>"); ?>
+                    <?php if (isset($this->errors["password"])) echo ("<br><span class='error'>" . $this->errors["password"] . "</span>"); ?>
                 </div>
                 <a class="blue-link" href="?action=register">Vous n'avez pas de compte ?</a>
                 <p><input type="submit" class="button" value="Enregister"></p>
-            </div>
+</form>
         </div>
         <!---->
     </div>

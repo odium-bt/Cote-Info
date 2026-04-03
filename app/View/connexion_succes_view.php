cec6d6e3bce1d5dad0a97fedd5481fed6cbe2db2
Zadig <odium@riseup.net>
2026-04-03 11:48:02 +0200

    added landing pages after register/login

---
+++ app/View/connexion_succes_view.php
@@ -0,0 +1,12 @@
+<main class="content">
+    <div class="flex">
+
+        <div class="box">
+            <h1>
+                Connexion réussie
+            </h1>
+
+        </div>
+
+    </div>
+</main>
--- app/View/inscription_succes_view.php
+++ app/View/inscription_succes_view.php
@@ -1,12 +1,12 @@
 <main class="content">
     <div class="flex">
-        <!-- Formulaire de connexion -->
+
         <div class="box">
             <h1>
                 Création de compte réussie
             </h1>
             <p>Vous pouvez maintenant vous connecter.</p>
         </div>
-        <!---->
+
     </div>
 </main>

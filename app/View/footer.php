3cf6315e44e4b0d39d304226577b560b73b28b5d
Zadig <odium@riseup.net>
2026-04-07 15:51:52 +0200

    footer created

--- app/View/footer.php
+++ app/View/footer.php
@@ -1,8 +1,45 @@
-<footer>
-
+<footer class="box">
+    <div class="footernav">
+        <div>
+            <h5>Côte Info</h5>
+            <p>Votre guide des stations balnéaires en France métropole.</p>
+        </div>
+        <div>
+            <h5>Liens rapides</h5>
+            <ul>
+                <a href="?action=rgpd">
+                    <li>RGPD</li>
+                </a>
+                <a href="?action=contact">
+                    <li>Contact</li>
+                </a>
+            </ul>
+        </div>
+        <div>
+            <h5>Communauté</h5>
+            <ul>
+                <a href="#">
+                    <li>Facebook</li>
+                </a>
+                <a href="#">
+                    <li>Twitter</li>
+                </a>
+                <a href="#">
+                    <li>Instagram</li>
+                </a>
+            </ul>
+        </div>
+    </div>
+    <hr>
+    <div>
+        <p class="copyright">&copy; 2026 Côte Info. Tous droits réservés.</p>
+    </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="./public/js/script.js"></script>
+<?php if (isset($_GET['action']) && $_GET['action'] === 'station') { ?>
+    <script src="./public/js/slider.js"></script>
+<?php } ?>
 </body>
 
 </html>

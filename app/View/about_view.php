bf8db869248c18eb5686af49b793723d73127b1e
Zadig <odium@riseup.net>
2026-04-10 08:55:12 +0200

    placeholder about page

---
+++ app/View/about_view.php
@@ -0,0 +1,13 @@
+<main class="content">
+    <div class="flex center">
+
+        <article class="box">
+            <h1 class="titre">A propos</h1>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Soluta odit quaerat, possimus quasi asperiores illum optio enim magni quam harum libero at dicta perferendis corrupti cumque suscipit commodi temporibus sint voluptatibus pariatur necessitatibus facilis itaque.
+                Dignissimos quae mollitia sint suscipit.</p>
+        </article>
+
+    </div>
+</main>

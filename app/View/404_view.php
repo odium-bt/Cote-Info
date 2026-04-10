8c409e1c2f7792fdf94003e0477d0e0308deaa6b
Zadig <odium@riseup.net>
2026-04-10 08:54:56 +0200

    updated 404

--- app/View/404_view.php
+++ app/View/404_view.php
@@ -1,3 +1,6 @@
 <main class="content">
-    <h1 class="titre">404, page introuvable</h1>
+    <article class="box">
+        <h1 class="titre">Erreur 404</h1>
+        <p>La page que vous avez demandé n'existe pas...</p>
+    </article>
 </main>

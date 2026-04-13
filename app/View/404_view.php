8064542c860332433a896a5b7705ba2ac10fef14
Zadig <odium@riseup.net>
2026-04-13 16:28:41 +0200

    updated 404

--- app/View/404_view.php
+++ app/View/404_view.php
@@ -1,5 +1,5 @@
 <main class="content">
-    <article class="box">
+    <article class="box padding-30 margin-side-20">
         <h1 class="titre">Erreur 404</h1>
         <p>La page que vous avez demandé n'existe pas...</p>
     </article>

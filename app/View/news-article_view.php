09d9886109743897d72e57552e5d986b87e02e2c
Zadig <odium@riseup.net>
2026-04-13 16:24:11 +0200

    + news article page

--- app/View/news-article_view.php
+++ app/View/news-article_view.php
@@ -1,16 +1,10 @@
 <main class="content">
     <div class="flex center">
 
-        <article id="news_list" class="box">
-            <h1 class="titre"><?= 'placeholder' ?></h1>
-            <section>
-                <?php
-                for ($i = 0; $i < sizeof($article['content']); $i++) {
-                ?>
-                    <p><?= $this->article['content'][$i] ?></p>
-                <?php
-                } ?>
-            </section>
+        <article class="article_news padding-50 box">
+
+            <h1 class="titre"><?= $this->article['title'] ?></h1>
+            <?= $this->article['content'] ?>
         </article>
 
     </div>

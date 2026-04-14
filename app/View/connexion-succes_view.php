ce8f85154fe5c2e2b9d65e21257ac15d02d49e6e
Zadig <odium@riseup.net>
2026-04-14 08:47:42 +0200

    new styling

--- app/View/connexion-succes_view.php
+++ app/View/connexion-succes_view.php
@@ -1,7 +1,7 @@
 <main class="content">
     <div class="flex center">
 
-        <div class="box">
+        <div class="box padding-30 margin-side-20">
             <h1 class="titre">
                 Connexion réussie
             </h1>

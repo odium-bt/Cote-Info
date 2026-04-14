ecadecee1c3fc86d1f1427a309ea4d4693e6572c
Zadig <odium@riseup.net>
2026-04-14 08:44:31 +0200

    + chronological sorting, + thumbnails method

--- app/Model/NewsModel.php
+++ app/Model/NewsModel.php
@@ -19,7 +19,7 @@ class NewsModel extends Model
     /*
      * Fonction getPreviews
      * paramètre : tableau d'ids
-     * résultat : tableau contenant l'id, le titre, la date et l'id thumbnail des articles demandés
+     * résultat : tableau contenant l'id, le titre, la date et l'id thumbnail des articles demandés, avec une limite de 6, par ordre décroissant de nouveauté
      */
     public function getPreviews($ids)
     {
@@ -27,8 +27,37 @@ class NewsModel extends Model
         $placeholders = implode(', ', array_fill(0, count($ids), '?'));
 
         return $this->dbRequestAll(
-            "SELECT `id_news`,`title`,`date_`,`id_thumbnail` FROM news WHERE id_news IN ($placeholders)",
+            "SELECT `id_news`,`title`,`date_`,`id_thumbnail` FROM news WHERE id_news IN ($placeholders) ORDER BY date_ DESC LIMIT 6",
             $ids
         ) ?? [];
+    }
+
+    /*
+     * Fonction getPreviews
+     * paramètre : tableau d'ids
+     * résultat : tableau contenant l'id, le titre, la date et l'id thumbnail des articles demandés
+     */
+    public function getThumbnails($articles)
+    {
+        $thumbnailIDs = array_column($articles, 'id_thumbnail');
+        if (empty($thumbnailIDs)) {
+            return $articles;
+        }
+
+        // Va chercher les infos dans la table media
+        $mediaModel = new MediaModel;
+        $thumbnails = $mediaModel->getMedias($thumbnailIDs);
+
+        // Range les thumbnails par leur IDs dans $thumbnailsByID
+        $thumbnailsByID = [];
+        foreach ($thumbnails as $thumbnail) {
+            $thumbnailsByID[$thumbnail['id_media']] = $thumbnail;
+        }
+        // Range les thumbnails dans leurs articles
+        foreach ($articles as &$article) {
+            $article['thumbnail'] = $thumbnailsByID[$article['id_thumbnail']] ?? null;
+        }
+
+        return $articles;
     }
 }

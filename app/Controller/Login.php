72b15940659ee0d377bcd4e3e5e7c2fd26ef3bec
Zadig <odium@riseup.net>
2026-04-20 10:44:04 +0200

    created a parent class for checking login info

--- app/Controller/Login.php
+++ app/Controller/Login.php
@@ -8,96 +8,25 @@ use CoteInfo\Model\UserModel;
  * Filtre les champs avant de refuser ou valider une connexion utilisateur
  */
 
-class Login
+class Login extends LoginForm
 {
-    public $errors = [];
-    protected string $email;
-    protected string $password;
-    protected int $counter = 0;
-
-    protected float $startTime = 0;
-    protected float $endTime = 0;
-    protected float $elapsedTime = 0;
-
     public function __construct()
     {
-        // Contrôle de qualité (trouve les erreurs et ajoute les messages dans le tableau $errors)
-        if (!empty($_POST)) {
-            foreach ($_POST as $key => $post_element) {
-                switch ($key) {
-                    case 'email':
-                        $this->email = strtolower(htmlspecialchars(trim($_POST['email']))) ?? null;
-                        break;
-                    case 'password':
-                        $this->password = htmlspecialchars(trim($_POST['password'])) ?? null;
-                        break;
-                }
-            }
+        parent::__construct();
 
-            // Email
-            if (!$this->email) {
-                $this->errors['email'] = "Requis";
-            }
-            // Mot de passe
-            if (!$this->password) {
-                $this->errors['password'] = "Requis";
-            }
-
-            if (!isset($user)) {
-                $user = new UserModel;
-            }
-            if (!$user->loginCheck($this->email, $this->password)) {
-                $this->errors['failure'] = "L'email ou le mot de passe que vous avez entrés sont erronés";
-                // Incrémente le compteur d'erreurs de connexion
-                $this->incrementCounter();
-            };
-        }
-        // ============ Sortie
         // Si $_POST est vide (début) ou qu'il y a des erreurs, affiche le formulaire
         if (empty($_POST) || !empty($this->errors)) {
             require ROOT . '/app/View/connexion_view.php'; // Retour au formulaire
         }
         // Sinon $_POST est rempli sans erreurs, affiche la page succès et connecte l'utilisateur
         else {
+            if (!isset($user)) {
+                $user = new UserModel;
+            }
             $_SESSION['user_id'] = $user->getIdByEmail($this->email);
             $_SESSION['is_admin'] = $user->isAdmin($_SESSION['user_id']);
             session_regenerate_id(true);
-            require ROOT . '/app/View/connexion-succes_view.php'; // Affiche page succès
+            require ROOT . '/app/View/connexion-success_view.php'; // Affiche page succès
         }
-        // ============        
-    }
-
-    /*
-     * Fonction incrementCounter
-     * paramètres : /
-     * résultat : si le compteur est à 10 ou +, ajoute une erreur
-     */
-    private function incrementCounter()
-    {
-        if ($this->counter >= 10) {
-            $this->errors['failure'] = "Trop de tentatives successives, merci d'attendre 10 minutes avant de réessayer";
-            if ($this->elapsedTime <= 0) {
-                $this->startTimer();
-            }
-        } else {
-            $this->counter += 1;
-        }
-    }
-
-    /*
-     * Fonction startTimer
-     * paramètres : /
-     * résultat : commence un décompte; après 10 minutes la variable counter retourne à 0
-     */
-    protected function startTimer()
-    {
-        $this->startTime = microtime(true);
-        while ($this->elapsedTime <= 600) {
-            $this->endTime = microtime(true);
-            $this->elapsedTime = $this->endTime - $this->startTime;
-            usleep(1000000); // waits 1 second
-        }
-        $this->elapsedTime = 0;
-        $this->counter = 0;
     }
 }

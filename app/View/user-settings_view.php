c0122399b512c13955d57d06e64b3219bda65931
Zadig <odium@riseup.net>
2026-04-20 16:05:28 +0200

    added disabled flag to darkmode switch and WIP text

--- app/View/user-settings_view.php
+++ app/View/user-settings_view.php
@@ -1,7 +1,9 @@
  <h2 class="margin-bottom-30">Préférences</h2>
  <div class="padding-30">
      <form class="flex column" action="?action=user&tab=settings" method="post">
-         <div><input type="checkbox" name="dark-mode"> <label for="dark-mode"> Mode sombre</label></div>
+         <div class="padding-30"><input type="checkbox" name="dark-mode" disabled> <label for="dark-mode"> Mode sombre</label>
+             <p>(WIP)</p>
+         </div>
          <div><input class="button" type="submit"></div>
      </form>
  </div>

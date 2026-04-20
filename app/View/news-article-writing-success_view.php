ebaf97555f04da23252c2d0b8977c1f05f3d2f47
Zadig <odium@riseup.net>
2026-04-20 11:38:58 +0200

    + added article writing success page

---
+++ app/View/news-article-writing-success_view.php
@@ -0,0 +1,14 @@
+<?php require ROOT . '/app/View/header_view.php'; ?>
+<main class="content">
+    <div class="flex justify-center">
+
+        <div class="box padding-30 margin-side-20">
+            <h1>
+                Création d'article réussie
+            </h1>
+            <p>Votre article a été mis en ligne.</p>
+        </div>
+
+    </div>
+</main>
+<?php require ROOT . '/app/View/footer_view.php'; ?>

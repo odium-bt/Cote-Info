0b3255dc5f5571cd3cb358e23d0cb617a391c724
Zadig <odium@riseup.net>
2026-04-21 15:23:53 +0200

    added admin button for article deletion

--- app/View/news-article_view.php
+++ app/View/news-article_view.php
@@ -3,8 +3,12 @@
     <div class="flex justify-center">
 
         <article class="article_news padding-50 box">
-            <h1><?= $this->article['title'] ?></h1>
-            <?= $this->article['content'] ?>
+            <h1><?= $this->currentArticle['title'] ?></h1>
+            <?= $this->currentArticle['content'] ?>
+
+            <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin'] === true) { ?>
+                <a class="bottom-left" href="?action=news&delete=<?= $_GET['id'] ?>" title="Supprimer l'article"><i class="fa-solid fa-trash"></i></a>
+            <?php } ?>
         </article>
 
     </div>

c67f1063b850438e8c7daa03ab56a628d7fb2632
Zadig <odium@riseup.net>
2026-04-21 15:27:36 +0200

    fixed avatar and added error msg

--- app/View/user-profile_view.php
+++ app/View/user-profile_view.php
@@ -1,8 +1,12 @@
  <h2 class="margin-bottom-30">Mon profil</h2>
+ <?php if (!empty($this->errors['avatar'])) { ?>
+     <p class="error"><?= $this->errors['avatar'] ?></p>
+ <?php } ?>
+
  <div class="padding-30 flex column">
      <div class="flex">
-         <img class="user_avatar margin-20" src="<?= ROOT . "/public/images/avatars/" . $this->user['avatar'] ?>" alt="Ton avatar" onerror="this.onerror=null; this.src='./public/images/avatars/default.png';">
-         <form action="?action=user" class="flex column">
+         <img class="user_avatar margin-20" src="./public/images/avatars/<?= $this->user['avatar'] ?>" alt="Ton avatar" onerror="this.onerror=null; this.src='./public/images/avatars/default.png';">
+         <form action="?action=user" class="flex column" enctype="multipart/form-data" method="post">
              <label class="margin-overunder-3" for="new-avatar">Modification de votre photo de profil : </label>
              <input class="margin-overunder-3" name="new-avatar" type="file">
              <input class="margin-overunder-3 button" type="submit">
